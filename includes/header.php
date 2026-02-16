96706f5c3634dcbf0a53da551b28801c62241cb4
Acidburn007 <sandesh.bishokarma@student.hamk.fi>
2026-02-16 22:27:24 +0545

    Logo path corrected

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,8 @@
 
   <!-- Tailwind CDN -->
   <script src="https://cdn.tailwindcss.com"></script>
+  <!--Icon CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
   <!-- Tailwind Config -->
   <script>
