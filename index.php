c3d1c0d98c8dda07a718d50cdc1f186acefd5094
Anupkandel-6335 <anupkandel6335@gmail.com>
2026-02-17 20:35:16 +0200

    Started Crud app and added README.md

--- index.php
+++ index.php
@@ -27,8 +27,14 @@ include 'includes/navbar.php';
             <a href="#services" class="px-8 py-4 bg-brand text-white rounded-xl shadow hover:scale-105 transition">
               Explore Services
             </a>
+
             <a href="#contact" class="px-8 py-4 border border-brand text-brand rounded-xl hover:bg-brand hover:text-white transition">
               Free Consultation
+
+            <a href="gettingstarted/gettingstarted.php" target="_blank"
+            class="px-8 py-4 border border-brand text-brand rounded-xl hover:bg-brand hover:text-white transition">
+              Start Your Journey
+
             </a>
           </div>
         </div>
