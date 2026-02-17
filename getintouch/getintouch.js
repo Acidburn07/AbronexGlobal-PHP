24d823ca47aa1e752a2c1117ef53ddbf2830a769
Acidburn007 <sandesh.bishokarma@student.hamk.fi>
2026-02-18 04:33:01 +0545

    Minor changes in code

--- getintouch/getintouch.js
+++ getintouch/getintouch.js
@@ -1,4 +1,4 @@
-<script>
+
 document.getElementById('quickContactForm').addEventListener('submit', function(event) {
     let isValid = true;
     if (document.getElementById('contact_name').value.trim() === "") { document.getElementById('contactNameError').classList.remove('hidden'); isValid = false; } else { document.getElementById('contactNameError').classList.add('hidden'); }
@@ -7,4 +7,3 @@ document.getElementById('quickContactForm').addEventListener('submit', function(
     if (document.getElementById('contact_message').value.trim() === "") { document.getElementById('contactMessageError').classList.remove('hidden'); isValid = false; } else { document.getElementById('contactMessageError').classList.add('hidden'); }
     if (!isValid) event.preventDefault(); 
 });
-</script>
